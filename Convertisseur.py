254d799170055f60dbf0189537b71fd6e7b769b9
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-08-26 13:21:07 +0300

    pyth2

--- Convertisseur.py
+++ Convertisseur.py
@@ -9,7 +9,7 @@ root = Tk()
  #variable
 
 root.title("Convertisseur Mp4 to Mp3 By Finoana Randri")
-root.config(background="Orange")
+root.config(background="#12EBCE")
 #root.iconbitmap("yad.ico")
 root.geometry("500x500")
 root.minsize(500,500)
@@ -17,7 +17,7 @@ root.minsize(500,500)
 width =400
 heigth =400
 image = PhotoImage(file="k.png").zoom(30).subsample(30)
-canvas = Canvas(root,width=width,height=heigth,bg="Orange",bd=0,highlightthickness=0)
+canvas = Canvas(root,width=width,height=heigth,bg="#12EBCE",bd=0,highlightthickness=0)
 canvas.create_image(width/2,heigth/2,image=image)
 canvas.pack(expand=YES)
 
@@ -25,7 +25,7 @@ def mp4():
     thefile = filedialog.askopenfilename(initialdir="/",title="chosose video",filetypes=(("mp4 files","*.mp4"), ("wav files","*.wav")))
     clip = mp.VideoFileClip(thefile)
     clip.audio.write_audiofile(thefile+".mp3")
-gradient = PhotoImage(file='f.png')
+gradient = PhotoImage(file='index.png')
 selectfile = Button(root,image=gradient,border=0,command=mp4).pack(expand=YES)
 
 
